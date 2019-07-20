ec5b6628954ca9ddc44cd9be9ddef2a8d7610869
hbvj99 <helloblazevijay99@gmail.com>
2019-07-20 20:28:26 +0545

    update save directory

--- us_embassy_kathmandu.py
+++ us_embassy_kathmandu.py
@@ -32,7 +32,7 @@ for item in url:
 browser.close() # browser exit
 
 # write lists to csv file
-with open('us_embassy_kathmandu.csv', 'w') as f:
+with open('data/us_embassy_kathmandu.csv', 'w') as f:
     writer = csv.writer(f)
     writer.writerows(zip(d_name, d_url))
 
